096002bc0cce807dc9563036124720288a43f051
programmerJ <programmerJ@outlook.com>
2021-10-06 23:24:09 -0400

    initialize

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TestingStuff
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //// Testing ++, --, +=, -=, =+, =-. The last two no longer exist/work (=+ & =-).
+            //var inputOne = 23;
+            //byte plusEquals = 17; // Defaults to 0.
+            //short minusEquals = 12; // Defaults to 0.
+            //long equalsPlus = 4; // Defaults to 0.
+            //int equalsMinus = 9; // Defaults to 0.
+            //float fourBytes = 1.5F; // Defaults to 0.
+            //double minusMinus = 7; // Defaults to 0.
+            //decimal cash = 7.9228M; // Defaults to 0.
+
+
+            //string bam; // Defaults to null.
+            //bool yes; // Defaults to false.
+            //inputOne++;
+            //Console.WriteLine($"What happens to 23 when you plus plus it? {(inputOne)}");
+
+            //minusMinus--;
+            //Console.WriteLine($"What happens to 7 when you minus minus it? {minusMinus}");
+
+            //plusEquals += 3;
+            //Console.WriteLine($"What happens when you take 17 and plus equals 3 to it? {plusEquals}");
+
+            //minusEquals -= 3;
+            //Console.WriteLine($"What happens when you take 12 and minus equals 3 to it? {minusEquals}");
+
+            //equalsPlus = +3;
+            //Console.WriteLine($"What happens when you take 4 and equals plus 3 to it? {equalsPlus}");
+
+            //equalsMinus = -10;
+            //Console.WriteLine($"What happens when you take 9 and equals minus 10 to it? {equalsMinus}");
+
+        }
+
+    }
+    
+}
